675befd01ae6f47396c253b13030821ec434371e
jhowcs <jhowcs@gmail.com>
2016-04-16 19:23:04 -0300

    I completely changed the home screen ui to new layout sent by the designers. Added a bottom toolbar. (Not finished yet, need to add map).

--- app/src/main/java/ca/com/androidbinnersproject/activities/home/HomeScreenFragment.java
+++ app/src/main/java/ca/com/androidbinnersproject/activities/home/HomeScreenFragment.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -30,66 +33,76 @@ import ca.com.androidbinnersproject.util.Util;
 
 public class HomeScreenFragment extends Fragment {
 
-	private ImageView imgProfile;
-    private TextView txtProfile;
-	private TabLayout tabLayout;
-	private ViewPager viewPager;
-
-	private Button newPickupButton;
+	private Toolbar mToolbarBottom;
 
 	public HomeScreenFragment() {
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
 		View view = inflater.inflate(R.layout.fragment_home_screen, container, false);
 
-		ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
-		adapter.AddFragment(new PickupListFragment(), "Upcoming Pick-Ups");
-		adapter.AddFragment(new PickupListFragment(), "Completed Pick-Ups");
-
-        txtProfile = (TextView) view.findViewById(R.id.home_screen_txtProfile);
-        imgProfile = (ImageView) view.findViewById(R.id.home_screen_imgProfile);
-		viewPager = (ViewPager) view.findViewById(R.id.home_viewpager);
-		viewPager.setAdapter(adapter);
-
-		tabLayout = (TabLayout) view.findViewById(R.id.home_tablayout);
-		tabLayout.setupWithViewPager(viewPager);
-
-		newPickupButton = (Button) view.findViewById(R.id.home_newpickup_button);
+	/*	newPickupButton = (Button) view.findViewById(R.id.home_newpickup_button);
 		newPickupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), PickupActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
 		return view;
 	}
 
-    @Override
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		mToolbarBottom = (Toolbar) view.findViewById(R.id.home_screen_include_toolbar);
+
+		setToolbatClickListener();
+
+		super.onViewCreated(view, savedInstanceState);
+	}
+
+	private void setToolbatClickListener() {
+		mToolbarBottom.findViewById(R.id.toolbar_bottom_btnHistory).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mToolbarBottom.findViewById(R.id.toolbar_bottom_btnOngoing).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mToolbarBottom.findViewById(R.id.toolbar_bottom_btnPickup).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), PickupActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		mToolbarBottom.findViewById(R.id.toolbar_bottom_btnNotifications).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mToolbarBottom.findViewById(R.id.toolbar_bottom_btnDonate).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+	}
+
+	@Override
     public void onStart() {
         super.onStart();
-
-        loadProfilePicture();
-        loadProfileName();
-    }
-
-    private void loadProfileName() {
-        txtProfile.setText(Util.getUserLogged(this.getActivity()));
-    }
-
-    private void loadProfilePicture() {
-        Glide.with(this).load(R.drawable.nopicture).asBitmap().into(new BitmapImageViewTarget(imgProfile) {
-            @Override
-            protected void setResource(Bitmap resource) {
-                RoundedBitmapDrawable circularBitmapDrawable =
-                        RoundedBitmapDrawableFactory.create(getActivity().getResources(), resource);
-                circularBitmapDrawable.setCircular(true);
-                imgProfile.setImageDrawable(circularBitmapDrawable);
-            }
-        });
     }
 }
