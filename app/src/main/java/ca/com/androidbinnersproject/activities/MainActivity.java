65e0fe74767ee4aac52df644129eb64a08d0f4dc
jhowcs <jhowcs@gmail.com>
2016-10-14 17:09:05 -0300

    Replace BottomNavigationBar (TabLayout) for a library that follow the Google Design Guidelines and has animations and more functionalities.

--- app/src/main/java/ca/com/androidbinnersproject/activities/MainActivity.java
+++ app/src/main/java/ca/com/androidbinnersproject/activities/MainActivity.java
@@ -11,17 +11,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 import ca.com.androidbinnersproject.R;
 import ca.com.androidbinnersproject.activities.home.HomeScreenFragment;
 import ca.com.androidbinnersproject.activities.ongoing.OngoingPickupsFragment;
 import ca.com.androidbinnersproject.activities.pickup.NewPickupFragment;
 import ca.com.androidbinnersproject.util.Util;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, LeftNavigationDrawerMenu.FragmentDrawerListener {
-    private FragmentManager mFragmentManager;
+public class MainActivity extends AppCompatActivity implements LeftNavigationDrawerMenu.FragmentDrawerListener,
+        BottomNavigationBar.OnTabSelectedListener {
     private Toolbar mToolbar;
     private LeftNavigationDrawerMenu mFragmentDrawer;
-    private TabLayout mTabLayout;
+    private BottomNavigationBar mBottomNavigationBar;
+
     private HomeScreenFragment mHomeScreenMapFragment;
 
     @Override
@@ -29,84 +33,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFragmentManager = getSupportFragmentManager();
-
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mFragmentDrawer = (LeftNavigationDrawerMenu) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        mBottomNavigationBar = (BottomNavigationBar) findViewById(R.id.home_screen_include_tabLayoutBottom);
+
+        final String userLogged = Util.getUserLogged();
+        mFragmentDrawer.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar, userLogged);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        final String userLogged = Util.getUserLogged();
-
-        mFragmentDrawer = (LeftNavigationDrawerMenu) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        mFragmentDrawer.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar, userLogged);
         mFragmentDrawer.setDrawerListener(this);
-
-        mTabLayout = (TabLayout) findViewById(R.id.home_screen_include_tabLayoutBottom);
-
         mHomeScreenMapFragment = new HomeScreenFragment();
 
-
+        createBottomNavigationTabs();
         selectDefaultTabIndex();
-        setTabLayoutClickListener();
-
         showHomeScreen();
+    }
+
+    private void createBottomNavigationTabs() {
+        mBottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        mBottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+        mBottomNavigationBar.setTabSelectedListener(this);
+
+        mBottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_buttom_history, getString(R.string.home_screen_toolbar_history_title)))
+                .addItem(new BottomNavigationItem(R.drawable.ic_buttom_ongoing, getString(R.string.home_screen_toolbar_ongoing_title)))
+                .addItem(new BottomNavigationItem(R.drawable.ic_buttom_pickup, getString(R.string.home_screen_toolbar_pickup_title)))
+                .addItem(new BottomNavigationItem(R.drawable.ic_buttom_donate, getString(R.string.home_screen_toolbar_donate_title)))
+                .setActiveColor(R.color.backgroundApp)
+                .initialise();
     }
 
     private void selectDefaultTabIndex() {
         //Select Pickup Tab by default
-        TabLayout.Tab tabAt = mTabLayout.getTabAt(2);
-        tabAt.select();
+        mBottomNavigationBar.setFirstSelectedPosition(2);
     }
 
     private void showHomeScreen() {
-        mFragmentManager.beginTransaction()
+        getSupportFragmentManager().beginTransaction()
                 .add(R.id.main_container_body, mHomeScreenMapFragment)
                 //.addToBackStack("home")
                 .commit();
     }
 
-    private void setTabLayoutClickListener() {
-        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                 showSelectedUI(tab.getPosition());
-            }
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else
+            super.onBackPressed();
+    }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
 
     }
 
-    private void showSelectedUI(final int position) {
+    @Override
+    public void onTabSelected(int position) {
         switch (position) {
             case 0: //History
                 break;
             case 1: //Ongoing
-                mFragmentManager.beginTransaction()
+                getSupportFragmentManager().beginTransaction()
                         .replace(R.id.main_container_body, OngoingPickupsFragment.newInstance(MainActivity.this))
                         .addToBackStack("ongoing")
                         .commit();
                 break;
             case 2: //New Pickup
-                mFragmentManager.beginTransaction()
+                getSupportFragmentManager().beginTransaction()
                         .replace(R.id.main_container_body, NewPickupFragment.newInstance(MainActivity.this,
-                                                                                         mHomeScreenMapFragment.getLatLng().latitude,
-                                                                                         mHomeScreenMapFragment.getLatLng().longitude))
+                                mHomeScreenMapFragment.getLatLng().latitude,
+                                mHomeScreenMapFragment.getLatLng().longitude))
                         .addToBackStack("pickup")
                         .commit();
 
                 break;
-            case 3: //Notifications
-                break;
-            case 4: //Donate
+            case 3: //Donate
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gifttool.com/donations/Donate?ID=1453&AID=503&PID=4805"));
                 startActivity(browserIntent);
                 break;
@@ -114,19 +118,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onBackPressed() {
-        if (mFragmentManager.getBackStackEntryCount() > 0) {
-            mFragmentManager.popBackStack();
-        } else
-            super.onBackPressed();
+    public void onTabUnselected(int position) {
+
     }
 
     @Override
-    public void onClick(View v) {
-    }
-
-    @Override
-    public void onDrawerItemSelected(View view, int position) {
+    public void onTabReselected(int position) {
 
     }
 }
